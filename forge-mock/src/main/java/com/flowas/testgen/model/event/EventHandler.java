117586ef49db3153a7f3fde93d914666922ada43
Liu Jianhong <flowas@gmail.com>
2010-12-18 08:37:54 +0800

    minor

--- forge-mock/src/main/java/com/flowas/testgen/model/event/EventHandler.java
+++ forge-mock/src/main/java/com/flowas/testgen/model/event/EventHandler.java
@@ -34,6 +34,10 @@ import net.flowas.codegen.resource.GenEnum;
 import net.flowas.codegen.resource.Settings;
 
 import org.jboss.seam.forge.parser.java.JavaClass;
+import org.jboss.seam.forge.project.Project;
+import org.jboss.seam.forge.project.dependencies.Dependency;
+import org.jboss.seam.forge.project.dependencies.DependencyBuilder;
+import org.jboss.seam.forge.project.facets.DependencyFacet;
 import org.w3c.dom.Element;
 
 import com.flowas.codegen.project.model.ExcuteEngine;
@@ -43,39 +47,42 @@ import com.flowas.testgen.model.ResourceRepository;
 public class EventHandler {
 	@Inject
 	ExcuteEngine engine;
+	@Inject
+	Project project;
+
 	/**
 	 * Add Mock code when matched a Depend on component
 	 * 
 	 * @param event
 	 */
-	public void onMatchedDoc(@Observes DocEvent event) {		
+	public void onMatchedDoc(@Observes DocEvent event) {
 		JavaClass cl = event.getUnderOperation();
 		Class<?> clasz;
 		try {
 			clasz = Class.forName(event.getClassName());
 			String mbody = "";
 			List<String> importList = new ArrayList<String>();
-			for (Method m : clasz.getDeclaredMethods()) {				
-				for (String mn : event.getMethodList()) {					
-					String name=mn;
+			for (Method m : clasz.getDeclaredMethods()) {
+				for (String mn : event.getMethodList()) {
+					String name = mn;
 					if (mn.contains("(")) {
-						name = mn.split("\\(")[0];						
+						name = mn.split("\\(")[0];
 					}
 					if (!name.equals(m.getName())) {
 						continue;
 					}
 					Class[] paraTypes = m.getParameterTypes();
 					String[] paras = methodParam(mn);
-					if(paraTypes.length!=paras.length){						
+					if (paraTypes.length != paras.length) {
 						continue;
 					}
-					boolean flag=false;
-					for(int i=0;i<paras.length;i++){
-						if(!paraTypes[i].getName().equals(paras[i].trim())){
-							flag=true;							
+					boolean flag = false;
+					for (int i = 0; i < paras.length; i++) {
+						if (!paraTypes[i].getName().equals(paras[i].trim())) {
+							flag = true;
 						}
 					}
-					if(flag){						
+					if (flag) {
 						continue;
 					}
 					String templateName = "PrivateNew";
@@ -101,16 +108,17 @@ public class EventHandler {
 			}
 			for (String imp : importList) {
 				cl.addImports(imp);
-			}			
-			for (org.jboss.seam.forge.parser.java.Method<JavaClass> m : cl.getMethods()) {
-				if(m.hasAnnotation("org.junit.Before")){
+			}
+			for (org.jboss.seam.forge.parser.java.Method<JavaClass> m : cl
+					.getMethods()) {
+				if (m.hasAnnotation("org.junit.Before")) {
 					m.setBody(mbody);
 				}
 			}
-		} catch (ClassNotFoundException e) {			
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
@@ -118,45 +126,70 @@ public class EventHandler {
 	 * 
 	 * @param event
 	 */
-	public void onDocDependent(@Observes DocEvent event) {		
-		if("java.lang.Runtime".equals(event.getClassName())){
+	public void onDocDependent(@Observes DocEvent event) {
+		if ("java.lang.Runtime".equals(event.getClassName())) {
 			DocumentBuilder bu;
 			try {
 				bu = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 				Element ma = bu.newDocument().createElement("maven");
-				ma.setAttribute("zip", "/META-INF/resources/achieve/" + event.getClassName()
-						+ ".zip");
+				ma.setAttribute("zip",
+						"/META-INF/resources/achieve/" + event.getClassName()
+								+ ".zip");
 				engine.maven(ma);
 			} catch (ParserConfigurationException e) {
 				e.printStackTrace();
 			}
-		}			
+		}
 	}
+
 	public void onTechDependent(@Observes TechEvent event) {
-		if(event.getName()==GenEnum.POWERMOCK){
+		if (event.getName() == GenEnum.POWERMOCK) {
 			JavaClass cl = event.getUnderOperation();
 			cl.addImport("org.powermock.modules.junit4.PowerMockRunner");
-			if(!cl.hasAnnotation("org.junit.runner.RunWith")){
-				cl.addAnnotation("org.junit.runner.RunWith").setLiteralValue("PowerMockRunner.class");
+			if (!cl.hasAnnotation("org.junit.runner.RunWith")) {
+				cl.addAnnotation("org.junit.runner.RunWith").setLiteralValue(
+						"PowerMockRunner.class");
 			}
-		}				
+			DependencyFacet maven = project.getFacet(DependencyFacet.class);
+			Dependency dependencyPJ = DependencyBuilder
+					.create("org.powermock:powermock-module-junit4:1.4.6:test");
+			if (!maven.hasDependency(dependencyPJ)) {
+				maven.addDependency(dependencyPJ);
+			}
+			Dependency dependencyP = DependencyBuilder
+					.create("org.powermock:powermock-api-easymock:1.4.6:test");
+			if (!maven.hasDependency(dependencyP)) {
+				maven.addDependency(dependencyP);
+			}
+			Dependency dependencyJ = DependencyBuilder
+					.create("junit:junit:4.8.2:test");
+			if (!maven.hasDependency(dependencyJ)) {
+				maven.addDependency(dependencyJ);
+			}
+			Dependency dependencyE = DependencyBuilder
+					.create("org.easymock:easymock:3.0:test");
+			if (!maven.hasDependency(dependencyE)) {
+				maven.addDependency(dependencyE);
+			}
+		}
 	}
+
 	private String[] methodParam(String mn) {
-    	String param="";
+		String param = "";
 		if (mn.contains("(")) {
 			String[] pair = mn.split("\\(");
-			mn =pair[0];
-			param= pair[1];
-			param=param.replace(")","");
+			mn = pair[0];
+			param = pair[1];
+			param = param.replace(")", "");
 		}
 		String[] paramArray;
-		if(param.trim().equals("")){
-			paramArray=new String[]{};
-		}else if(param.contains(",")){
+		if (param.trim().equals("")) {
+			paramArray = new String[] {};
+		} else if (param.contains(",")) {
 			paramArray = param.split(",");
-		}else{
-			paramArray=new String[]{param};
+		} else {
+			paramArray = new String[] { param };
 		}
 		return paramArray;
-    }
+	}
 }
